2fd0c24ae42fd18b5bda511e5059d2141a493e6b
Cab18 <anacarolinanascimentobrito@gmail.com>
2021-09-17 23:49:42 -0300

    Conhecimentos aprendidos

---
+++ MetodoGetset.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author carol
+ */
+public class MetodoGetset{
+
+     String jnome;
+
+    public void setJnome(string a){
+        this.jnome=a;
+    }
+
+  public String getJnome(){
+         String Jnome = null;
+      return Jnome;
+      
+      cm.setJnome(nome);
+    System.out.println("seu nome é"+cm.getJnome());
+    
+      
+  }
+  }
+     
